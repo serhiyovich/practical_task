9c2ee9ea09032d0883a8830470b8015ded18c529
Vitalii Shramko <vitalii_shramko@epam.com>
2018-03-29 15:19:09 +0300

    all delete

--- practical_task/src/main/java/autoamtionframework/waiter/Waiter.java
+++
@@ -1,9 +0,0 @@
-package autoamtionframework.waiter;
-
-import org.openqa.selenium.WebElement;
-
-public interface Waiter {
-	void waitForElement(WebElement element);
-
-//	void waitForElement(By by);
-}
--- practical_task/src/main/java/testdata/TestData.java
+++
@@ -1,9 +0,0 @@
-package testdata;
-
-public class TestData {
-	public static final String EMAIL = "testforvitaliishramko@gmail.com";
-	public static final String PASSWORD = "prodigy100";
-	public static final String SUBJECT_TEXT = "Important";
-	public static final String MESSAGE_TEXT = "I work in EPAM Systems";
-	public static final String URL = "https://mail.google.com";
-}
